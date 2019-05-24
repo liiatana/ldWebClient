3db2767abdbed204dc0a0dad0f1165fc191a3293
liiatana <31036379+liiatana@users.noreply.github.com>
2019-05-24 13:45:26 +0300

    Добавлен класс для хранения папки Сообщения

---
+++ wcTests/src/test/java/ru/lanit/ld/wc/model/instructionFolder.java
@@ -0,0 +1,39 @@
+package ru.lanit.ld.wc.model;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import java.util.Objects;
+
+public class instructionFolder {
+
+    private int id;//: 1999
+    private String name; //: "Входящая"
+
+    public instructionFolder(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public instructionFolder(JsonElement parsed) {
+
+        JsonObject jsonFolder = parsed.getAsJsonObject().get("folder").getAsJsonObject();
+
+        this.id = jsonFolder.get("id").getAsInt();
+        this.name = jsonFolder.get("name").getAsString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        instructionFolder that = (instructionFolder) o;
+        return id == that.id &&
+                Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
